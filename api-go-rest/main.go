0fc1fee377cffa36cd44a197dc92d4a63f32333a
Rafael Wainer <rafael.wainer@gmail.com>
2023-09-17 16:06:48 -0300

    feat:GET Id unico

--- api-go-rest/main.go
+++ api-go-rest/main.go
@@ -9,10 +9,12 @@ import (
 func main() {
 	models.Personalidades = []models.Personalidade{
 		{
+			Id:       1,
 			Nome:     "Nome 1",
 			Historia: "Historia 1",
 		},
 		{
+			Id:       2,
 			Nome:     "Nome 2",
 			Historia: "Historia 2",
 		},
--- api-go-rest/routes/routes.go
+++ api-go-rest/routes/routes.go
@@ -1,9 +1,11 @@
 package routes
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/rafawainer/golang_api_rest/api-go-rest/models"
 	"net/http"
+	"strconv"
 )
 
 func HandleRequest() {
@@ -22,11 +24,38 @@ func HandleRequest() {
 
 	r.GET("/api/personalidades/:id", func(c *gin.Context) {
 		id := c.Param("id")
-		c.JSON(http.StatusOK, gin.H{
-			"id":     id,
-			"status": http.StatusOK,
-		})
-	})
+		idInt, err := strconv.Atoi(id)
+		naoNumerico := idInt < 1
+		if err != nil || naoNumerico {
+			message := "Erro para converter o id " + id + " para numerico"
+			fmt.Println(message)
+			c.JSON(http.StatusBadRequest, gin.H{
+				"id":      id,
+				"status":  http.StatusBadRequest,
+				"message": message,
+			})
+		} else {
+			p := models.Personalidades
+			r, message := models.RetornaUmaPersonalidade(p, idInt)
+			if r.Id > 0 {
+				c.JSON(http.StatusOK, gin.H{
+					"id":      id,
+					"data":    r,
+					"status":  http.StatusOK,
+					"message": message,
+				},
+				)
+			} else {
+				c.JSON(http.StatusOK, gin.H{
+					"id":      id,
+					"status":  http.StatusOK,
+					"message": message,
+				},
+				)
+			}
+		}
+	},
+	)
 
 	//r.http.HandleFunc(
 	//	"/",
