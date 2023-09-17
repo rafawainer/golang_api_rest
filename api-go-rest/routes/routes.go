fa35d113a3c9a06f5ddb02f9cb8555a418918b5d
Rafael Wainer <rafael.wainer@gmail.com>
2023-09-17 13:13:59 -0300

    feat: json GET response

--- api-go-rest/routes/routes.go
+++ api-go-rest/routes/routes.go
@@ -1,19 +1,41 @@
 package routes
 
 import (
-	"github.com/rafawainer/golang_api_rest/api-go-rest/controllers"
-	"log"
+	"github.com/gin-gonic/gin"
+	"github.com/rafawainer/golang_api_rest/api-go-rest/models"
 	"net/http"
 )
 
 func HandleRequest() {
-	http.HandleFunc(
-		"/",
-		controllers.Home,
-	)
-	http.HandleFunc(
-		"/api/personalidades",
-		controllers.TodasPersonalidades,
-	)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Home Page")
+	})
+
+	r.GET("/api/personalidades", func(c *gin.Context) {
+		p := models.Personalidades
+		c.JSON(http.StatusOK, gin.H{
+			"response": p,
+			"status":   http.StatusOK,
+		})
+	})
+
+	r.GET("/api/personalidades/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		c.JSON(http.StatusOK, gin.H{
+			"id":     id,
+			"status": http.StatusOK,
+		})
+	})
+
+	//r.http.HandleFunc(
+	//	"/",
+	//	controllers.Home,
+	//)
+	//http.HandleFunc(
+	//	"/api/personalidades",
+	//	controllers.TodasPersonalidades,
+	//)
+	r.Run(":8000")
+	//log.Fatal(http.ListenAndServe(":8000", nil))
 }
